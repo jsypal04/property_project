3838001adbf5137455fb3deef19ecec45005cabf
Joseph Sypal <60202320+jsypal04@users.noreply.github.com>
2022-07-11 15:40:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,23 @@
 import tkinter as tk
-import sqlite3
-
-def block(base):
-    con = sqlite3.connect('database/properties.db')
-    cur = con.cursor()
-
-    get_data.block(cur, con, base)
-
-    con.close()
 
 # gets the results then puts displays them using labels
 def disp_results(results):
-    props = predictor.predict()
+    lbl = tk.Label(results, text="This action returns a list of the potenital good investments")
+    lbl.pack()
+    result_lbls.append(lbl)
 
-    r = 1
-    for prop in props:
-        btn = tk.Button(results, text="Block", command=lambda: block(prop))
-        n = util.float_to_str(round(prop[1], 0))
-        lbl = tk.Label(results, text=prop[0][4] + ": " + n)
-        btn.grid(row=r, column=0)
-        lbl.grid(row=r, column=1)
-        result_lbls.append(lbl)
-        result_btns.append(btn)
-        r += 1
+    back_button.pack()
 
     controls.pack_forget()
-    results.grid(padx=20, pady=20)
+    results.pack(padx=20, pady=20)
 
 # goes back to the controls frame
 def back():
+    back_button.pack_forget()
     for i in range(len(result_lbls)):
         result_lbls[i].destroy()
-        result_btns[i].destroy()
 
-    results.grid_forget()
+    results.pack_forget()
     controls.pack(padx=50, pady=50)
 
 def success_back():
@@ -41,9 +25,7 @@ def success_back():
     controls.pack(padx=50, pady=50)
 
 def update():
-    update_db.update()
-
-    success_frame.pack()
+    success_frame.pack(padx=20, pady=20)
     controls.pack_forget()
 
 # creates the window
@@ -51,7 +33,6 @@ root = tk.Tk()
 
 # a variable to hold the result labels and buttons
 result_lbls = []
-result_btns = []
 
 # the three frames that the user can be on
 controls = tk.Frame(root)
@@ -62,13 +43,12 @@ success_frame = tk.Frame(root)
 predict_button = tk.Button(controls, text="Predict", command=lambda: disp_results(results))
 update_db_button = tk.Button(controls, text="Update Database", command=update)
 back_button = tk.Button(results, text="Back", command=back)
-success_lbl = tk.Label(success_frame, text="Success!")
+success_lbl = tk.Label(success_frame, text="This action pulls Bright MLS listing data to analyze")
 success_btn = tk.Button(success_frame, text="Back", command=success_back)
 
 # packs all of the widgets
 predict_button.pack(pady=10)
 update_db_button.pack(pady=10)
-back_button.grid(row=0, column=0, columnspan=2)
 success_lbl.pack()
 success_btn.pack()
 
